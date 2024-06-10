4b57710a8ae09c7345a5e77307cbba965b4ac095
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-06-10 15:28:00 +0600

    added session transaction

--- src/app/modules/courses/course.service.ts
+++ src/app/modules/courses/course.service.ts
@@ -1,8 +1,11 @@
+import mongoose from "mongoose";
 import QueryBuilder from "../../builder/QueryBuilder";
 import { CouserSearchAbleField } from "./course.constant";
 import { TCourses } from "./course.interface";
 import { Courses } from "./course.model";
 import { course } from "./course.router";
+import { AppError } from "../../errors/AppErrors";
+import httpStatus from "http-status";
 
 const createCourseInToDB = async (paylod: TCourses) => {
   const result = await Courses.create(paylod);
@@ -45,33 +48,74 @@ const DeleteCourseFromDB = async (id: string) => {
 const updateCourseInToDB = async (id: string, paylod: Partial<TCourses>) => {
   const { perRequisteCourses, ...CourseInfo } = paylod;
   // console.log(id);
-  const updateCourse = await Courses.findByIdAndUpdate(id, CourseInfo, {
-    new: true,
-    runValidators: true,
-  });
-  console.log(perRequisteCourses);
-  if (perRequisteCourses?.length > 0) {
-    const deletePerRequiste=perRequisteCourses?.filter(el=>el.courses &&el?.isDeleted).map(el=>el?.courses)
-   
-//filter out deleted field
-    const deletePerRequisteCourses= await Courses.findByIdAndUpdate(id,
-      {
-        $pull:{perRequisteCourses:{courses:{$in:deletePerRequiste}}}
-      },{
-        new:true
-      }
-    )
-    const newPerRequiste=perRequisteCourses?.filter(el=>el.courses && el?.isDeleted !=true)
-    
-    const newPerRequisteCourses= await Courses.findByIdAndUpdate(id,
-      {
-        $addToSet:{perRequisteCourses:{$each:newPerRequiste}}
-      }
-    )
+  const session = await mongoose.startSession();
 
+  try {
+    session.startTransaction();
+
+    const updateCourse = await Courses.findByIdAndUpdate(id, CourseInfo, {
+      new: true,
+      runValidators: true,
+      session,
+    });
+    if (!updateCourse) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Failed to update course");
+    }
+    // console.log(perRequisteCourses);
+    if (perRequisteCourses && perRequisteCourses?.length > 0) {
+      const deletePerRequiste = perRequisteCourses
+        ?.filter((el) => el.courses && el?.isDeleted)
+        .map((el) => el?.courses);
+
+      //filter out deleted field
+      const deletePerRequisteCourses = await Courses.findByIdAndUpdate(
+        id,
+        {
+          $pull: {
+            perRequisteCourses: { courses: { $in: deletePerRequiste } },
+          },
+        },
+        {
+          new: true,
+          session,
+        }
+      );
+      if (!deletePerRequisteCourses) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Failed to update course");
+      }
+      const newPerRequiste = perRequisteCourses?.filter(
+        (el) => el.courses && el?.isDeleted != true
+      );
+
+      //added new courses
+      const newPerRequisteCourses = await Courses.findByIdAndUpdate(
+        id,
+        {
+          $addToSet: { perRequisteCourses: { $each: newPerRequiste } },
+        },
+        {
+          new: true,
+          session,
+        }
+      );
+      if (!newPerRequisteCourses) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Failed to update course");
+      }
+    }
+
+    const result = await Courses.findById(id).populate(
+      "perRequisteCourses.courses"
+    );
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return result;
+  } catch (error) {
+    await session.abortTransaction()
+    await session.endSession()
+    throw new AppError(httpStatus.BAD_REQUEST, "Failed to update course");
   }
-  const result=await Courses.findById(id).populate("perRequisteCourses.courses")
-  return result;
 };
 
 export const CourseServices = {
